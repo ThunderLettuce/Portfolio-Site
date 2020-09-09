06458011d2bdd9329e9ca024b74ae408e2c05b19
Sam <pixelspammy@gmail.com>
2020-09-09 16:26:57 -0700

    Added login method

--- portfolio/auth.py
+++ portfolio/auth.py
@@ -28,14 +28,13 @@ def register():
         # Validate that username is not already registered by querying the database and checking if a result is returned.
         elif db.execute(
             'Select id FROM user WHERE username = ?', (username,)
-        # fetchone() returns one row from the query.
+            # fetchone() returns one row from the query.
         ).fetchone() is not None:
         error = f'User {username} is already registered'
 
         if error is None:
             # If validation succeeds, insert the new user data into the database.
             # generate_password_hash() is used to securely hash the password, and that hash is stored.
-            # 
             db.execute(
                 'INSERT INTO user (username, password) VALUES (?, ?)',
                 (username, generate_password_hash(password))
@@ -44,10 +43,40 @@ def register():
             db.commit()
             # After storing the user, they are redirected to the login page.
             return redirect(url_for(auth.login))
-        
+
         # If validation fails, the error is shown to the user.
         # flash() stores messages that can be retrieved when rendering the template.
         flash(error)
 
     # When the user initially navigates to auth/register, or there was a validation error, an HTML page with the registration form should be shown.
     return render_template('auth/register.html')
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        # The user is queried first and stored in a variable for later use.
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        # check_password_hash() hashes the submitted password in the same way as the stored hash and securely compares them. If they match, the password is valid.
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+        
+        # session is a dict that stores data across requests.
+        if error is None:
+            session.clear()
+            # When validation succeeds, the user’s id is stored in a new session.
+            # The data is stored in a cookie that is sent to the browser, and the browser then sends it back with subsequent requests.
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+    
+    return render_template('auth/login.html')
