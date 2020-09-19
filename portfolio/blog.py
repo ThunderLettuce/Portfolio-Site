212bab2278a0e7ea177904a3dd458890a53eb203
Sam <pixelspammy@gmail.com>
2020-09-19 13:12:31 -0700

    added create method

--- portfolio/blog.py
+++ portfolio/blog.py
@@ -5,6 +5,7 @@ from flaskr.db import get_db
 
 bp = Blueprint('blog', __name__)
 
+
 @bp.route('/')
 def index():
     db = get_db()
@@ -14,3 +15,29 @@ def index():
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
+
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            erro - 'Title is required'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
+            )
+            bd.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/create.html')
